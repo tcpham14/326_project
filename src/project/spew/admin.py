2f2aef87cb8f8adf80fdf477d6ce649a5f0616c2
Lucas Phan <lucasphan@umass.edu>
2018-11-06 16:06:31 -0500

    admin added classes under prof

--- src/project/spew/admin.py
+++ src/project/spew/admin.py
@@ -30,7 +30,7 @@ class SubjectAdmin(admin.ModelAdmin):
     # model. Fields are displayed vertically by default, but will
     # display horizontally if you further group them in a tuple as we
     # do here for the birth and death dates.
-
+    
     inlines = [ClassInline]
 
 
@@ -56,7 +56,7 @@ class UserAdmin(admin.ModelAdmin):
     # display horizontally if you further group them in a tuple as we
     # do here for the birth and death dates.
     fields = ["first_name", "last_name", "major"]
-
+    list_filter = ("first_name", "last_name")
     inlines = [FeedbackInline]
 
 
@@ -70,6 +70,9 @@ class UserAdmin(admin.ModelAdmin):
 #    model = Professor
 
 
+class MembershipInline(admin.TabularInline):
+    model = Professor.course.through
+
 @admin.register(Class)
 class ClassAdmin(admin.ModelAdmin):
     # By setting the list_display variable in an Admin class will have
@@ -79,7 +82,7 @@ class ClassAdmin(admin.ModelAdmin):
     # a costly operation when accessing the database. So, we have it
     # display the results of a function call (display_genre) - see the
     # defintion of this function in the Book class in models.py.
-    list_display = ("title", "class_id", "subject", "code") #need to add the professor
+    list_display = ("title", "class_id", "subject", "code", "attendance", "exams") #need to add the professor
     fields = ["title"]
     # This allows us to display information about the corresponding
     # book instances of this book. It is clearly useful to be able to
@@ -87,13 +90,13 @@ class ClassAdmin(admin.ModelAdmin):
     # BookInstance model defines a "foreign key" on Book, Django will
     # automatically be able to look up the associated book instances.
     # inlines = [ProfessorInline]
-    inlines = [FeedbackInline]
+    inlines = [FeedbackInline, MembershipInline]
 
 
 
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "position")
+    list_display = ("first_name", "last_name", "contact")
 
     # By setting the list_filter variable in an Admin class it will be
     # used to populate a "filter" UI box component in the admin site
@@ -106,11 +109,13 @@ class ProfessorAdmin(admin.ModelAdmin):
     # tuple is the title of the section (None, if no title), followed
     # by a dictionary containing the entry "fields" that correspond to
     # the fields the section will have.
-    # inlines = [ClassInline]
+    inlines = [MembershipInline]
+    exclude = ('course',)
     
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ("course", "user", "rating", "date", "comment")
-
+    list_filter = ("course", "user")
     # inlines = [UserInline]
     
+
