52b32cb5ea115731bed12fafe921a2502d5d1020
Lucas Phan <lucasphan@umass.edu>
2018-11-06 17:10:22 -0500

    lucas atomic

--- src/project/spew/migrations/0001_initial.py
+++ src/project/spew/migrations/0001_initial.py
@@ -6,7 +6,7 @@ import uuid
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
     initial = True
 
     dependencies = [
--- src/project/spew/migrations/0002_auto_20181106_2202.py
+++ src/project/spew/migrations/0002_auto_20181106_2202.py
@@ -4,7 +4,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
     dependencies = [
         ('spew', '0001_initial'),
     ]
