abeb2c1b470e1dc42b1e30251f1a7594feb40c8b
thanh <thanhpham@umass.edu>
2018-11-26 18:58:06 -0500

    updated forms, login and registration page

--- src/project/spew/forms.py
+++ src/project/spew/forms.py
@@ -4,20 +4,25 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First name', 'style': 'margin-top: 7px'}))
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name', 'style': 'margin-top: 7px'}))
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'email@example.com', 'style': 'margin-top: 7px'}))
+    password1 = forms.Field(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password', 'style': 'margin-top: 7px'}))
+    password2 = forms.Field(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password', 'style': 'margin-top: 7px'}))
+
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username', 'style': 'margin-top: 7px'}),        }
 
 class LoginForm(AuthenticationForm):
     
     username = forms.CharField(label=("Username"), max_length=30, 
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(label=("Username"), max_length=30, 
-                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
  
 
 # class StudentForm(forms.ModelForm):
